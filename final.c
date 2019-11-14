97ae8923bed61caa2457a1e98cea8cc970fe2b65
junhyung9985 <48814228+junhyung9985@users.noreply.github.com>
2019-11-14 20:27:34 +0900

    Update final.c

--- final.c
+++ final.c
@@ -6,11 +6,20 @@
 char flag = 0 ;
 void interrupt_handler () {
 	flag++ ;
-	printf("flag: %d\n", flag) ; }
+	printf("flag: %d\n", flag) ;
+	if( flag %2 ==0){ 
+		digitalWrite (0, HIGH) ; delay (500) ;
+	}
+	else{
+		digitalWrite (0, LOW) ; delay (500) ;
+	}
+}
 int main () {
 	if (wiringPiSetup() < 0) exit(EXIT_FAILURE) ;
+	pinMode(0,OUTPUT);
 	if (wiringPiISR(22, INT_EDGE_FALLING, &interrupt_handler) < 0) {
-    		fprintf(stderr, "unsable to setup ISR\n") ;
+   
+		fprintf(stderr, "unsable to setup ISR\n") ;
 		exit(1) ;
 	 }
 	while (1) ;
